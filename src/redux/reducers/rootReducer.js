777cc19cf34534384507cb6fa33f82202bae52f3
Bukhari47 <Mera543210>
2021-12-13 16:35:43 +0500

    removed redux files

--- src/redux/reducers/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import UsersDetails from "./usersReducer";
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({ UsersDetails });
-
-export default rootReducer;
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,12 +1,5 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import userReducer from "./reducers/usersReducer";
 import userDetails from "./slice/userSlice";
 import { configureStore } from "@reduxjs/toolkit";
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const store = createStore(userReducer, composeEnhancer(applyMiddleware(thunk)));
 
 export const store = configureStore({
   reducer: {
